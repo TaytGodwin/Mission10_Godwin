9da19b0f7928403ceb7ccaafe340e340f68bebab
taterg99 <taterg99@gmail.com>
2025-03-10 23:11:11 -0600

    Finishing touches

--- frontend/src/Bowlers.tsx
+++ frontend/src/Bowlers.tsx
@@ -2,15 +2,15 @@ import { useEffect, useState } from 'react';
 import { Bowler } from './types/Bowler';
 
 function BowlerList() {
-  const [bowlers, setBowlers] = useState<Bowler[]>([]);
+  const [bowlers, setBowlers] = useState<Bowler[]>([]); // Set bowlers to an empty array if not retrieved
 
   useEffect(() => {
     const fetchBowlers = async () => {
       const response = await fetch('https://localhost:5000/api/Bowler');
       const data = await response.json();
-      setBowlers(data);
+      setBowlers(data); // Set bowlers data after retrieving it from the api
     };
-    fetchBowlers();
+    fetchBowlers(); // Runs function above or else it won't work
   }, []);
 
   return (
@@ -28,19 +28,23 @@ function BowlerList() {
           </tr>
         </thead>
         <tbody>
-          {bowlers.map((b) => (
-            <tr key={b.bowlerId}>
-              <td>
-                {b.bowlerFirstName} {b.bowlerMiddleInit} {b.bowlerLastName}
-              </td>
-              <td>{b.teamName}</td>
-              <td>{b.bowlerAddress}</td>
-              <td>{b.bowlerCity}</td>
-              <td>{b.bowlerState}</td>
-              <td>{b.bowlerZip}</td>
-              <td>{b.bowlerPhoneNumber}</td>
-            </tr>
-          ))}
+          {bowlers.map(
+            (
+              b // Iterates through each bowler
+            ) => (
+              <tr key={b.bowlerId}>
+                <td>
+                  {b.bowlerFirstName} {b.bowlerMiddleInit} {b.bowlerLastName}
+                </td>
+                <td>{b.teamName}</td>
+                <td>{b.bowlerAddress}</td>
+                <td>{b.bowlerCity}</td>
+                <td>{b.bowlerState}</td>
+                <td>{b.bowlerZip}</td>
+                <td>{b.bowlerPhoneNumber}</td>
+              </tr>
+            )
+          )}
         </tbody>
       </table>
     </>
--- frontend/src/types/Bowler.ts
+++ frontend/src/types/Bowler.ts
@@ -1,4 +1,5 @@
 export type Bowler = {
+  // This is already formatted with a DTO on the backend to be able to return this information
   bowlerId: number;
   bowlerLastName: string;
   bowlerFirstName: string;
